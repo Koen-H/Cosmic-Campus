87a4d8fe89d1fe85adbaa11f3e1e060884a73f02
Koen H <49477924+Koen-H@users.noreply.github.com>
2023-05-15 14:45:42 +0200

    ADded health to the character

--- Assets/PlayerCharacterController.cs
+++ Assets/PlayerCharacterController.cs
@@ -1,17 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
+
+/// <summary>
+/// This is the player Character that is used to walk and attack within the game.
+/// </summary>
 public class PlayerCharacterController : NetworkBehaviour
 {
+    //NetworkVariables
+    NetworkVariable<float> health = new(10);
+    //LocalVariables
     public float moveSpeed = 5f;
+
     [SerializeField] private GameObject playerAvatar;
+    [SerializeField] TextMeshPro healthText;
+
+
+    public override void OnNetworkSpawn()
+    {
+        InitCharacter(OwnerClientId);
+        health.OnValueChanged += OnHealthChange;
+    }
+
+    void OnHealthChange(float prevHealth, float newHealth)
+    {
+        healthText.text = health.ToString();
+        if (prevHealth > newHealth)//Do thing where the player takes damage!
+        {
+            Debug.Log("Take damage!");
+        }
+        else if (prevHealth < newHealth)//Do things where the player gained health!
+        {
+            Debug.Log("Gained healht!");
+        }
+        else { Debug.LogError("Networking error?"); }
+    }
+
+
     void Update()
     {
-        if (!IsOwner)
-            return;
+        if (!IsOwner) return;//Things below this should only happen on the client!
+
+        Move();
+    }
+
+    private void Move()
+    {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -30,10 +68,8 @@ public class PlayerCharacterController : NetworkBehaviour
         Debug.Log(playerData.avatarId.Value);
         GameObject newAvatar = playerData.playerRoleData.GetAvatar(playerData.avatarId.Value);//Get the new avatar
         Instantiate(newAvatar, playerAvatar.transform);
+        healthText.text = health.ToString();
+
     }
 
-    public override void OnNetworkSpawn()
-    {
-        InitCharacter(OwnerClientId);
-    }
 }
