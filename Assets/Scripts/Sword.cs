0b37ba5bd75b2620b8c7ad7eeca83bb1f68c7e3a
Omair <513589@student.saxion.nl>
2023-05-16 15:16:51 +0200

    Player can be damaged and enemy also

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class Sword : Weapon
 {
-    protected override void Attack()
+    public override void Attack()
     {
         base.Attack();
 
 
-    }
+        // calculate raycast direction
+        Vector3 rayDirection = transform.TransformDirection(Vector3.forward);
 
+        Debug.DrawRay(transform.position + Vector3.up, rayDirection, Color.red, 0.01f);
+        // initialize a variable to store the hit information
+        RaycastHit hit;
+
+        // shoot the raycast
+        if (Physics.Raycast(transform.position + Vector3.up, rayDirection, out hit, 1))
+        {
+            // check if the object hit has the tag "Enemy"
+            if (hit.transform.CompareTag("Enemy"))
+            {
+                // call DealDamage function
+                DealDamage(hit.transform.gameObject);
+            }
+        }
+    }
+    void DealDamage(GameObject enemy)
+    {
+        enemy.transform.parent.GetComponent<Enemy>().TakeDamage(damage);
+    }
 }
