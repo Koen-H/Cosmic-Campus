408f7f68264b785665f15bab0788413b85d49988
Omair <513589@student.saxion.nl>
2023-06-13 16:36:30 +0200

    Better player movement

--- Assets/Scripts/RoomInfo.cs
+++ Assets/Scripts/RoomInfo.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class RoomInfo : MonoBehaviour
@@ -7,6 +8,7 @@ public class RoomInfo : MonoBehaviour
     public Transform doorEntrance;
     public Transform doorExit;
 
+    [HideInInspector] public int roomLayer;
 
     public Vector3 normalEntrance;
     public Vector3 normalExit;
@@ -20,5 +22,20 @@ public class RoomInfo : MonoBehaviour
     public Vector3 GetTeacherPosition() => teacherSpawnPoint.position;
 
     public List<EnemySpawner> enemySpawners= new List<EnemySpawner>();
+
+    public List<NetworkObject> potions = new List<NetworkObject>();
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerCharacterController player = other.GetComponent<PlayerCharacterController>();
+        if (player)
+        {
+            RoomGenerator.Instance.SpawnEnemiesInRoomServerRpc(roomLayer);
+            player.checkPoint = doorEntrance.position;
+        }
+
+
+    }
 
 }
