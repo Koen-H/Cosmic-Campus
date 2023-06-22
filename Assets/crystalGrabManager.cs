599444186a81a6ee9a5be964c75f31344b0423f9
Koen H <49477924+Koen-H@users.noreply.github.com>
2023-06-22 15:39:12 +0200

    crystal grab now directly goes to the player

--- Assets/crystalGrabManager.cs
+++ Assets/crystalGrabManager.cs
@@ -14,23 +14,26 @@ public class crystalGrabManager : MonoBehaviour
 
     private void Start()
     {
-        Vector3 targetDirection = transform.position - staffObject.position;
-        transform.rotation = Quaternion.LookRotation(targetDirection.normalized, Vector3.up);
+        //Vector3 targetDirection = transform.position - staffObject.position;
+        //transform.rotation = Quaternion.LookRotation(targetDirection.normalized, Vector3.up);
+        
     }
 
 
     private void Update()
     {
-        timer += Time.deltaTime; 
+        timer += Time.deltaTime;
         // Calculate the direction to look at the staffObject
-        Vector3 targetDirection = staffObject.position - transform.position;
+        transform.LookAt(staffObject.position);
+        transform.position = transform.position +( transform.forward * (speed * Time.deltaTime));
+        //Vector3 targetDirection = staffObject.position - transform.position;
         //Quaternion targetRotation = Quaternion.LookRotation(targetDirection.normalized, Vector3.up);
 
         // Rotate the object towards the target rotation with the desired speed
         //transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, (rotationSpeed + (rotationSpeed/(1+timer))) * Time.deltaTime);
 
         // Move the object forward
-        transform.Translate(transform.forward * (speed * Time.deltaTime));
+        //transform.Translate(transform.forward * (speed * Time.deltaTime));
 
         // Check if the object is close to the staffObject and destroy it if necessary
         if ((transform.position - staffObject.position).magnitude < 0.3f)
