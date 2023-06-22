d924a1e52e71b93124e16917ca3f521e656a2452
Omair <513589@student.saxion.nl>
2023-06-22 15:38:00 +0200

    Added Arrow thing, gonna fix later

---
+++ Assets/Scripts/UiDirectionIndicatorManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiDirectionIndicatorManager : MonoBehaviour
+{
+    [SerializeField] UiDirectionIndicator deathPrefab;
+    [SerializeField] Camera cam;
+    [SerializeField] Canvas canvas; 
+
+
+
+
+}
