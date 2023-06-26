1a5665177ff5028460849a42f7356bc0825a322b
Omair <513589@student.saxion.nl>
2023-06-26 10:52:31 +0200

    Networked studnet Animations

--- Assets/Scripts/QuestStudentNPC.cs
+++ Assets/Scripts/QuestStudentNPC.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Unity.Netcode;
 
 public class QuestStudentNPC : QuestNPC
 {
@@ -9,14 +10,25 @@ public class QuestStudentNPC : QuestNPC
 
     [SerializeField] Animator studnetAnimator;
     bool running;
+    [ClientRpc]
+    private void ToggleRunningClientRpc(bool isRunning)
+    {
+        running = isRunning;
+        studnetAnimator.SetBool("Running", running);
+    }
 
 
     private void Update()
     {
+        if (!IsServer) return;
         bool temp = running; 
         if (agent.desiredVelocity.magnitude > 0) running = true;
         else running = false;
 
-        if (temp != running) studnetAnimator.SetBool("Running", running);
+        if (temp != running) {
+            if(IsServer) ToggleRunningClientRpc(running);
+            studnetAnimator.SetBool("Running", running);
+
+        }
     }
 }
