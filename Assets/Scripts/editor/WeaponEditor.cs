7fa0b35f9d3d1dad37eca5eda1e33cfbc4a1e445
Koen H <49477924+Koen-H@users.noreply.github.com>
2023-06-28 20:46:35 +0200

    Editor scritp for weapons

--- Assets/Scripts/editor/WeaponEditor.cs
+++ Assets/Scripts/editor/WeaponEditor.cs
@@ -2,44 +2,130 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-[CustomEditor(typeof(WeaponData))]
 
+[CustomEditor(typeof(WeaponData))]
 public class WeaponEditor : Editor
 {
+    private WeaponData weapon;
+    private SerializedProperty weaponTypeProperty;
+    private SerializedProperty nickNameProperty;
+    private SerializedProperty weaponPrefabProperty;
+    private SerializedProperty weaponObjOffsetProperty;
+    private SerializedProperty damageProperty;
+    private SerializedProperty damageHealProperty;
+    private SerializedProperty speedProperty;
+    private SerializedProperty cooldownProperty;
+    private SerializedProperty rangeProperty;
+    private SerializedProperty projectilePrefabProperty;
+    private SerializedProperty maxChargeTimeProperty;
+    private SerializedProperty chargeProjectileSpeedProperty;
+    private SerializedProperty chargeProjectileDamageProperty;
+    private SerializedProperty accuracyProperty;
+    private SerializedProperty beamVfxProperty;
 
-        private WeaponData weapon;
+    private void OnEnable()
+    {
+        weapon = (WeaponData)target;
+        weaponTypeProperty = serializedObject.FindProperty("weaponType");
+        nickNameProperty = serializedObject.FindProperty("nickName");
+        weaponPrefabProperty = serializedObject.FindProperty("weaponPrefab");
+        weaponObjOffsetProperty = serializedObject.FindProperty("weaponObjOffset");
+        damageProperty = serializedObject.FindProperty("damage");
+        damageHealProperty = serializedObject.FindProperty("damageHeal");
+        speedProperty = serializedObject.FindProperty("speed");
+        cooldownProperty = serializedObject.FindProperty("cooldown");
+        rangeProperty = serializedObject.FindProperty("range");
+        projectilePrefabProperty = serializedObject.FindProperty("projectilePrefab");
+        maxChargeTimeProperty = serializedObject.FindProperty("maxChargeTime");
+        chargeProjectileSpeedProperty = serializedObject.FindProperty("chargeProjectileSpeed");
+        chargeProjectileDamageProperty = serializedObject.FindProperty("chargeProjectileDamage");
+        accuracyProperty = serializedObject.FindProperty("accuracy");
+        beamVfxProperty = serializedObject.FindProperty("beamVfx");
+    }
 
-        private void OnEnable()
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUI.BeginChangeCheck();
+
+        EditorGUILayout.PropertyField(weaponTypeProperty);
+
+        if (EditorGUI.EndChangeCheck())
         {
-            weapon = (WeaponData)target;
+            serializedObject.ApplyModifiedProperties();
+            serializedObject.Update();
         }
 
-
-        public override void OnInspectorGUI()
+        switch (weapon.weaponType)
         {
-
-            base.OnInspectorGUI();
-
-            serializedObject.Update();
-
-            switch (weapon.weaponType)
-            {
             case WeaponType.SWORD:
-                   // enemy.meleeRange = EditorGUILayout.FloatField(nameof(enemy.meleeRange), enemy.meleeRange);
-                    break;
+                EditorGUILayout.PropertyField(nickNameProperty);
+                EditorGUILayout.PropertyField(weaponPrefabProperty);
+                EditorGUILayout.PropertyField(weaponObjOffsetProperty);
+                EditorGUILayout.PropertyField(damageProperty);
 
-                case WeaponType.BOW:
-                    //enemy.projectileType = (EnemySO.ProjectileType)EditorGUILayout.EnumPopup("Projectile Type", enemy.projectileType);
-                    //enemy.projectileSpeed = EditorGUILayout.FloatField(nameof(enemy.projectileSpeed), enemy.projectileSpeed);
-                    break;
+                EditorGUILayout.PropertyField(damageHealProperty);
+
+
+                break;
+            case WeaponType.BOW:
+                EditorGUILayout.PropertyField(nickNameProperty);
+                EditorGUILayout.PropertyField(weaponPrefabProperty);
+                EditorGUILayout.PropertyField(weaponObjOffsetProperty);
+                EditorGUILayout.PropertyField(damageProperty);
+
+               // EditorGUILayout.PropertyField(speedProperty);
+                EditorGUILayout.PropertyField(cooldownProperty);
+
+                EditorGUILayout.PropertyField(projectilePrefabProperty);
+                EditorGUILayout.PropertyField(maxChargeTimeProperty);
+                EditorGUILayout.PropertyField(chargeProjectileSpeedProperty);
+                EditorGUILayout.PropertyField(chargeProjectileDamageProperty);
+
+                break;
             case WeaponType.STAFF:
-                //enemy.projectileType = (EnemySO.ProjectileType)EditorGUILayout.EnumPopup("Projectile Type", enemy.projectileType);
-                //enemy.projectileSpeed = EditorGUILayout.FloatField(nameof(enemy.projectileSpeed), enemy.projectileSpeed);
+                EditorGUILayout.PropertyField(nickNameProperty);
+                EditorGUILayout.PropertyField(weaponPrefabProperty);
+                EditorGUILayout.PropertyField(weaponObjOffsetProperty);
+                EditorGUILayout.PropertyField(damageProperty);
+                EditorGUILayout.PropertyField(cooldownProperty);
+
+                EditorGUILayout.PropertyField(rangeProperty);
+                EditorGUILayout.PropertyField(accuracyProperty);
+                EditorGUILayout.PropertyField(beamVfxProperty);
+
+
                 break;
             default:
-                //hide all
-                    break;
-            }
-            serializedObject.ApplyModifiedProperties();
+                HideAllFields();
+                break;
         }
+
+        serializedObject.ApplyModifiedProperties();
     }
+
+    private void HideAllFields()
+    {
+        EditorGUI.BeginChangeCheck();
+
+        weaponTypeProperty.enumValueIndex = EditorGUILayout.Popup("Weapon type", weaponTypeProperty.enumValueIndex, weaponTypeProperty.enumDisplayNames);
+
+        if (EditorGUI.EndChangeCheck())
+            serializedObject.ApplyModifiedProperties();
+
+        EditorGUI.BeginChangeCheck();
+
+        var serializedProperties = serializedObject.GetIterator();
+        while (serializedProperties.NextVisible(true))
+        {
+            if (serializedProperties.name != "m_Script" && serializedProperties.name != "weaponType")
+            {
+                EditorGUILayout.PropertyField(serializedProperties, true);
+            }
+        }
+
+        if (EditorGUI.EndChangeCheck())
+            serializedObject.ApplyModifiedProperties();
+    }
+}
